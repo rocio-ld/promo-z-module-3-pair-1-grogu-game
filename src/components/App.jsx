03ce46adc1459afa0bbc7d5c093c97b297368ccb
rocio_ld <rocio_ld@hotmail.com>
2024-09-04 11:22:51 +0200

    scss e index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,52 @@
-
+//importar imagenes
 
 function App() {
-
+//constantes 
 
 
   
   return (
-    <>
-   
-        <h1 className='title'>Grogu Game</h1>
-        </>  
+  <div className="page">
+    <header>
+      <h1>¡Cuidado con Grogu!</h1>
+    </header>
+    <main className="page">
+      <section className="board">
+        <div className="cell"><div className="grogu">👣</div></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </section>
+
+      <section>
+        <button className="dice">Lanzar Dado</button>
+        <div className="game-status">En curso</div>
+      </section>
+
+      <section className="goods-container">
+        <div className="goods-item">🍪</div>
+        <div className="goods-item">🍪</div>
+        <div className="goods-item">🍪</div>
+      </section>
+      <section className="goods-container">
+        <div className="goods-item">🥚</div>
+        <div className="goods-item">🥚</div>
+        <div className="goods-item">🥚</div>
+      </section>
+      <section className="goods-container">
+        <div className="goods-item">🐸</div>
+        <div className="goods-item">🐸</div>
+        <div className="goods-item">🐸</div>
+      </section>
+      <section>
+        <button className="restart-button">Reiniciar Juego</button>
+      </section>
+    </main>
+        
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
-import './scss/index.scss';
+//import './scss/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
